310d02ee034363c57674b2131ba3139d0aaed605
nazar12314 <kononenkonazar47@gmail.com>
2022-05-13 15:33:27 +0300

    completed search

--- Trie.py
+++ Trie.py
@@ -1,15 +1,19 @@
+from typing import Dictionary
+
+
 class Node:
-    def __init__(self, terminal: bool = False) -> None:
+    def __init__(self, data=None, terminal: bool = False) -> None:
         """
         arg: children -> dictionary of children of the given Node
         arg: terminal -> boolean value to check whether a node is an end of the word
         """
-        self.terminal = terminal
-        self.children = {}
+        self.terminal: bool = terminal
+        self.data: str = data
+        self.children: Dictionary = {}
 
 
 class Trie:
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Creates a root for a Trie
         """
@@ -25,11 +29,11 @@ class Trie:
 
         for char in word:
             if char not in temporary.children:
-                temporary.children[char] = Node()
+                temporary.children[char] = Node(data=char)
             temporary = temporary.children[char]
         temporary.terminal = True
 
-    def search(self, word: str) -> bool:
+    def exists(self, word) -> bool:
         """Checks word for a presence in a tree
 
         Args:
@@ -46,7 +50,42 @@ class Trie:
             temporary = temporary.children[char]
         return True
 
-    def height(self, node=None):
+    def search(self, query: str) -> list:
+        """Searches for words that match given query
+
+        Args:
+            query (str): given query
+
+        Returns:
+            list: list of words that match given query
+        """
+        if not self.exists(query):
+            return []
+
+        result = []
+        temporary: Node = self._root
+
+        for char in query:
+            temporary = temporary.children[char]        
+
+        def depth_first_search(node: Node, prefix: str) -> None:
+            """Helper function for iterating through the deep of a tree.
+
+            Args:
+                node (Node): a starting point
+                prefix (str): a string that has already been formed
+            """
+            if node.terminal:
+                result.append(prefix + node.data)
+
+            for child in node.children.values():
+                depth_first_search(child, prefix + node.data)
+
+        depth_first_search(temporary, query[:-1])
+
+        return result
+
+    def height(self, node=None) -> int:
         """
         Returns height of the tree
         """
@@ -56,22 +95,24 @@ class Trie:
             return 0
         else:
             return 1 + max(self.height(node.children[child]) for child in node.children)
-        
+
+    def delete(self, word: str) -> bool:
+        pass
 
     def __str__(self) -> str:
         """
         String representation of a tree
         """
-        def recurse(node: Node, level: int) -> str:
+        def recursive_helper(node: Node, level: int) -> str:
             """
             Recursive helper function for str
             """
             result = ""
             if node is not None:
                 for child in node.children:
-                    result += recurse(node.children[child], level + 1)
+                    result += recursive_helper(node.children[child], level + 1)
                     result += "| " * level
                     result += str(child) + "\n"
             return result
 
-        return recurse(self._root, 0)
+        return recursive_helper(self._root,  0)
