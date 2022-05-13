bd955c96b2a3ac6f491d0ba9d52f9055a92ce8dd
nazar12314 <kononenkonazar47@gmail.com>
2022-05-13 14:15:13 +0300

    Added str repr and height func to the Trie module

--- Trie.py
+++ Trie.py
@@ -15,20 +15,63 @@ class Trie:
         """
         self._root = Node()
 
-    def insert(self, text: str) -> None:
+    def insert(self, word: str) -> None:
+        """Inserts a word into the Trie
+
+        Args:
+            word (str): a word to insert
+        """
         temporary: Node = self._root
 
-        for char in text:
+        for char in word:
             if char not in temporary.children:
                 temporary.children[char] = Node()
             temporary = temporary.children[char]
         temporary.terminal = True
 
-    def contains(self, text: str):
+    def search(self, word: str) -> bool:
+        """Checks word for a presence in a tree
+
+        Args:
+            word (str): a word to search for
+
+        Returns:
+            Boolean: whether a word is present in the tree
+        """
         temporary = self._root
 
-        for char in text:
+        for char in word:
             if char not in temporary.children:
                 return False
             temporary = temporary.children[char]
         return True
+
+    def height(self, node=None):
+        """
+        Returns height of the tree
+        """
+        if node is None:
+            node = self._root
+        if not node.children:
+            return 0
+        else:
+            return 1 + max(self.height(node.children[child]) for child in node.children)
+        
+
+    def __str__(self) -> str:
+        """
+        String representation of a tree
+        """
+        def recurse(node: Node, level: int) -> str:
+            """
+            Recursive helper function for str
+            """
+            result = ""
+            if node is not None:
+                for child in node.children:
+                    result += recurse(node.children[child], level + 1)
+                    result += "| " * level
+                    result += str(child) + "\n"
+            return result
+
+        return recurse(self._root, 0)
